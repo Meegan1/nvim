c5c09dfe233a18bfbadc43fe9027a84a7ea4a461
Meegan1 <jake@meegan.me>
2026-06-25 22:58:57 +0100

    feat: add LspLog command to open LSP log file and update ts_ls to use lspconfig defaults

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -57,6 +57,22 @@ return {
 				on_attach = on_attach,
 			})
 		end,
+		after = function()
+			local function open_lsp_log()
+				local ok, fname = pcall(function()
+					return vim.lsp.log.get_filename()
+				end)
+				if not ok or not fname or fname == "" then
+					vim.notify("nvim-lspconfig: LSP log not available", vim.log.levels.WARN)
+					return
+				end
+				vim.cmd("tabnew " .. vim.fn.fnameescape(fname))
+			end
+
+			vim.api.nvim_create_user_command("LspLog", function(info)
+				return open_lsp_log()
+			end, { nargs = "*", desc = "Open the LSP log file" })
+		end,
 	},
 	{
 		"lazydev.nvim",
@@ -126,56 +142,6 @@ return {
 			lspEnable(name)
 		end,
 		lsp = {
-			init_options = { hostInfo = "neovim" },
-			cmd = { "typescript-language-server", "--stdio" },
-			filetypes = {
-				"javascript",
-				"javascriptreact",
-				"typescript",
-				"typescriptreact",
-			},
-			root_markers = { "tsconfig.json", "jsconfig.json", "package.json", ".git" },
-			handlers = {
-				-- handle rename request for certain code actions like extracting functions / types
-				["_typescript.rename"] = function(_, result, ctx)
-					local client = assert(vim.lsp.get_client_by_id(ctx.client_id))
-					vim.lsp.util.show_document({
-						uri = result.textDocument.uri,
-						range = {
-							start = result.position,
-							["end"] = result.position,
-						},
-					}, client.offset_encoding)
-					vim.lsp.buf.rename()
-					return vim.NIL
-				end,
-			},
-			commands = {
-				["editor.action.showReferences"] = function(command, ctx)
-					local client = assert(vim.lsp.get_client_by_id(ctx.client_id))
-					local file_uri, position, references = unpack(command.arguments)
-
-					local quickfix_items = vim.lsp.util.locations_to_items(references, client.offset_encoding)
-					vim.fn.setqflist({}, " ", {
-						title = command.title,
-						items = quickfix_items,
-						context = {
-							command = command,
-							bufnr = ctx.bufnr,
-						},
-					})
-
-					vim.lsp.util.show_document({
-						uri = file_uri,
-						range = {
-							start = position,
-							["end"] = position,
-						},
-					}, client.offset_encoding)
-
-					vim.cmd("botright copen")
-				end,
-			},
 			on_attach = function(client, bufnr)
 				on_attach(client, bufnr)
 
@@ -193,11 +159,32 @@ return {
 
 					vim.lsp.buf.code_action({
 						context = {
-							diagnostics = {},
 							only = source_actions,
+							diagnostics = {},
 						},
 					})
 				end, {})
+
+				-- Go to source definition command
+				vim.api.nvim_buf_create_user_command(bufnr, "LspTypescriptGoToSourceDefinition", function()
+					local win = vim.api.nvim_get_current_win()
+					local params = vim.lsp.util.make_position_params(win, client.offset_encoding)
+					client:exec_cmd({
+						command = "_typescript.goToSourceDefinition",
+						title = "Go to source definition",
+						arguments = { params.textDocument.uri, params.position },
+					}, { bufnr = bufnr }, function(err, result)
+						if err then
+							vim.notify("Go to source definition failed: " .. err.message, vim.log.levels.ERROR)
+							return
+						end
+						if not result or vim.tbl_isempty(result) then
+							vim.notify("No source definition found", vim.log.levels.INFO)
+							return
+						end
+						vim.lsp.util.show_document(result[1], client.offset_encoding, { focus = true })
+					end)
+				end, { desc = "Go to source definition" })
 
 				vim.keymap.set("n", "<leader>ir", function()
 					vim.lsp.buf.code_action({
